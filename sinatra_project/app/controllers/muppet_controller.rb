dce327ddabaafd1d2e23cca078a5091a77d0b617
Luke Gill <bagrad007@gmail.com>
2021-03-01 15:05:17 -0500

    finish CRUD routes

--- sinatra_project/app/controllers/muppet_controller.rb
+++ sinatra_project/app/controllers/muppet_controller.rb
@@ -14,8 +14,25 @@ class MuppetController < ApplicationController
   end
 
   get "/muppets/:id" do
-    # binding.pry
     @muppet = Muppet.find_by(id: params[:id])
     erb :"muppets/show_muppet"
+  end
+
+  get "/muppets/:id/edit" do
+    @muppet = Muppet.find_by(id: params[:id])
+    erb :"muppets/edit"
+  end
+
+  patch "/muppets/:id" do
+    binding.pry
+    @muppet = Muppet.find_by(id: params[:id])
+    @muppet.update(name: params[:name], job: params[:job])
+    redirect to "/muppets/#{@muppet.id}"
+  end
+
+  delete "/muppets/:id/delete" do
+    muppet = Muppet.find_by(id: params[:id])
+    muppet.delete
+    redirect to "/muppets"
   end
 end
