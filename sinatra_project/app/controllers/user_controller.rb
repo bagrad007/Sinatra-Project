471acbb1aab6be8e2f04a531e66cc65e954b06dd
Luke Gill <bagrad007@gmail.com>
2021-03-03 15:33:50 -0500

    checks off spec list

--- sinatra_project/app/controllers/user_controller.rb
+++ sinatra_project/app/controllers/user_controller.rb
@@ -26,7 +26,6 @@ class Usercontroller < ApplicationController
   end
 
   post "/login" do
-    binding.pry
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
